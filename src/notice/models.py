99ed8bc45afdbe65fb357718ee97f71bf67a88de
wonjongin <wji0227@naver.com>
2024-09-09 22:40:49 +0900

    [FEAT] Insert ocr_data col into Notice model

--- src/notice/models.py
+++ src/notice/models.py
@@ -9,10 +9,11 @@ class Notice(models.Model):
     description = models.TextField()
     notice_id = models.IntegerField()
     date = models.DateField()
-    tdindex = models.TextField() #타이틀+내용 같이있는 텍스트
+    tdindex = models.TextField() # 타이틀+내용 같이있는 텍스트
     imgs = models.JSONField()
     links = models.JSONField()
     attachments = models.JSONField()
+    ocr_data = models.JSONField() # OCR 된 데이터를 담는 곳 (이미지 배열 순서대로 문자열로 넣기)
     base_url = models.URLField()
     univ_code = models.CharField(max_length=50) # 학교 코드
     org_code = models.CharField(max_length=50) # 부속기관|단과대 코드
